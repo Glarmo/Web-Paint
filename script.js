af64e06e77ec577917c8d3c5fa4e526222e224e5
glarmo <guillermVillalobos@hotmail.com>
2023-06-02 14:41:26 +0100

    Finalised features

--- script.js
+++ script.js
@@ -3,19 +3,68 @@ const smallCanvas = document.getElementById("small");
 const mediumCanvas = document.getElementById("medium");
 const largeCanvas = document.getElementById("large");
 const canvas = document.getElementById("canvas");
+const title = document.getElementById("title");
+const colorPicker = document.getElementById("colorPicker");
+const removeLines = document.getElementById("grid");
+
+let drawing = true;
+let erasing = false;
+let currentColor = "#FF0000";
+
+smallCanvas.addEventListener("click", createCanvas(100));
+
+mediumCanvas.addEventListener("click", createCanvas(400));
+
+largeCanvas.addEventListener("click", createCanvas(900));
+
+colorPicker.addEventListener("change", changeColor);
+
+removeLines.addEventListener("click", toggleGridLines);
+
+canvas.oncontextmenu = function () {
+  erasing = !erasing;
+  return false;
+};
+
+canvas.onmousedown = function (event) {
+  if (event.button == 0) {
+    drawing = !drawing;
+  }
+};
+
+onmouseup = function () {
+  if (drawing) {
+    drawing = !drawing;
+  }
+  if (erasing) {
+    erasing = !erasing;
+  }
+};
+
+let onLoad = createCanvas(100);
+onLoad();
 
 function enableDrawing() {
+  drawing = !drawing;
+}
+
+function preparePixels() {
   for (let i = 0; i < pixels.length; i++) {
     pixels[i].addEventListener("mouseover", colorIn);
+    pixels[i].addEventListener("mousedown", colorIn);
   }
 }
+
+function changeColor(event) {
+  currentColor = event.target.value;
+}
+
 enableDrawing();
 function createCanvas(size) {
   return function () {
-    console.log(canvas.innerHTML);
-    canvas.innerHTML = ""
-    let root = Math.sqrt(size/100);
-    let columns = (size/10) / root;
+    canvas.innerHTML = "";
+    let root = Math.sqrt(size / 100);
+    let columns = size / 10 / root;
     for (let i = 0; i < size; i++) {
       //create pixel and append to canvas
       const pixel = document.createElement("div");
@@ -25,17 +74,31 @@ function createCanvas(size) {
       pixel.style.height = "18px";
     }
     canvas.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-    enableDrawing();
+    preparePixels();
   };
 }
 
-function colorIn() {
-  console.log("hover");
-  this.style.backgroundColor = "red";
+function colorIn(event) {
+  if (drawing || (event.type == "mousedown" && event.button == 0)) {
+    this.style.backgroundColor = currentColor;
+  } else if (erasing) {
+    this.style.backgroundColor = "transparent";
+  }
 }
 
-smallCanvas.addEventListener("click", createCanvas(100));
+function toggleGridLines() {
+  if (pixels[0].style.border == "none") {
+    for (let i = 0; i < pixels.length; i++) {
+      pixels[i].style.border = "1px dotted grey";
+    }
+  } else {
+    for (let i = 0; i < pixels.length; i++) {
+      pixels[i].style.border = "none";
+    }
+  }
+}
 
-mediumCanvas.addEventListener("click", createCanvas(400));
-
-largeCanvas.addEventListener("click", createCanvas(900));
+function getColor() {
+  let color = title.style.color;
+  return color;
+}
